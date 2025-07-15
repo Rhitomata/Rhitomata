b751327abb0459c7c09f4be01042cab082188def
Rhitomaic <53262904+Rhitomaic@users.noreply.github.com>
2025-07-15 19:20:13 +0700

    Added EditorToolbox

---
+++ Assets/Scripts/Rhitomata/InputManager.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Rhitomata
+{
+    /// <summary>
+    /// Having new input system mixed with old input system is very confusing actually
+    /// </summary>
+    public class InputManager
+    {
+        public void Update()
+        {
+        }
+    }
+}
