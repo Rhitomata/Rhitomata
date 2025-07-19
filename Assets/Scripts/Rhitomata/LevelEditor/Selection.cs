9bfd1369aa93ca52864e5c49d2e617ccae44f4cc
Rhitomaic <53262904+Rhitomaic@users.noreply.github.com>
2025-07-19 14:37:29 +0700

    Update Selection.cs

--- Assets/Scripts/Rhitomata/LevelEditor/Selection.cs
+++ Assets/Scripts/Rhitomata/LevelEditor/Selection.cs
@@ -18,11 +18,10 @@ namespace Rhitomata
             if (!enableSelecting) return;
 
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
 
             ISelectable hovered = null;
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 hovered = hit.collider.GetComponent<ISelectable>();
             }
